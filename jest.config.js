fa6cecdf0eafa1ac9826161d311dae51eb788a6a
Vinicius Campos <vinicamposdev@gmail.com>
2023-02-18 18:48:10 -0300

    ci: 👷 add test on ci

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: ['**/src/**/*'],
+  "moduleFileExtensions": [ "js", "ts" ],
+  "coveragePathIgnorePatterns": [
+    "node_modules",
+    "jestGlobalMocks.ts",
+    ".module.ts",
+    ".mock.ts"
+  ],
 };
