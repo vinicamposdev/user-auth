e6003e41dbac77061c957565b5e897210e880612
Vinicius Campos <vinicamposdev@gmail.com>
2023-02-19 21:38:59 -0300

    refactor: 🚚 move from src to src/main

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['tests/**/*'],
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
   "moduleFileExtensions": [ "js", "ts" ],
   "coveragePathIgnorePatterns": [
     "node_modules",
